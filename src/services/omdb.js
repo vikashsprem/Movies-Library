b0a9149cb18f3069c86532af66f2616c7af3a316
Vikash Prem Sharma <vikashkumarukg977@gmail.com>
2024-06-01 17:38:26 +0530

    Fix a bug

--- src/services/omdb.js
+++ src/services/omdb.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
-const BASE_URL = 'http://www.omdbapi.com/';
+const BASE_URL = 'https://www.omdbapi.com/';
 
 export const searchMovies = (query) => {
   return axios.get(`${BASE_URL}?t=${query}&apikey=${API_KEY}`);
